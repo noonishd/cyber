b464c759bcb98d24963e45d9db8255c52585350a
noonishd <166075228+noonishd@users.noreply.github.com>
2025-04-03 09:15:31 -0700

    Update List_users.py now can delete and promote

--- List_users.py
+++ List_users.py
@@ -1,48 +1,74 @@
 
-from datetime import datetime
 from main import run, doubleprint
-import pytz
+import time
 
-doubleprint(f'\n\n\n\n\n\nLog created: {datetime.now(pytz.timezone("Canada/Pacific")).strftime("%I:%M:%S:%p")}\n\n\nUsers:\n\n\n')
+def main1():
+  doubleprint(f'\n\n\nUsers:\n\n\n')
 
 #-----------------------------Checking_Users---------------------------------------
 
-valid_shells = run('cat /etc/shells')[1:]
+  valid_shells = run('cat /etc/shells')[1:]
 
-users = run('cat /etc/passwd')
+  users = run('cat /etc/passwd')
 
-doubleprint(f"Valid Shells in /etc/shells:\n{valid_shells}\n\n\n\n")
+  doubleprint(f"Valid Shells in /etc/shells:\n{valid_shells}\n\n\n\n")
 
-for user in users:
-  user = user.split(':')
-  if user[len(user)-1] in valid_shells:
-    doubleprint(f"                      '{user[0]}' in valid shell")
-    if int(user[2]) == 0 and int(user[3]) == 0:
-      doubleprint('                      Root User!\n')
-    elif int(user[2]) < 1000:
-      doubleprint('                      Hidden User!\n')
-    elif int(user[2]) >= 1000:
-      doubleprint('                      Valid User\n')
+  for user in users:
+    user = user.split(':')
+    if user[len(user)-1] in valid_shells:
+      doubleprint(f"                      '{user[0]}' in valid shell")
+      if int(user[2]) == 0 and int(user[3]) == 0:
+        doubleprint('                      Root User!\n')
+      elif int(user[2]) < 1000:
+        doubleprint('                      Hidden User!\n')
+      elif int(user[2]) >= 1000:
+        doubleprint('                      Valid User\n')
+      doubleprint('                      Valid User\n', sendtext=False)
+      
+      admin_check = ('adm' in run(f'groups {user[0]}')[0].split())
 
-  else:
-    doubleprint(f"'{user[0]}' not in a login shell\n")
+      if admin_check:
+        if 'n' == input(f'"{user[0]}" IS an admin. Should they be an admin? (y/n): ').lower(): #-----------first check
 
+          if 'y' == input(f'Demote "{user[0]}"? (y/n): ').lower(): #------------Are you sure?
+            run(f'sudo deluser {user[0]} adm')
+            doubleprint(f'            {user[0]} DEMOTED from admin\n') #--------------done
+      else:
+        if 'y' == input(f'"{user[0]}" ISNT an admin. Should they be an admin? (y/n): ').lower(): #-----------first check
+
+          if 'y' == input(f'Promote "{user[0]}"? (y/n): ').lower(): #------------Are you sure?
+            run(f'sudo adduser {user[0]} adm')
+            doubleprint(f'            {user[0]} PROMOTED to admin\n') #--------------done
+
+      print(f'Should "{user[0]}" be deleted????? (y/n): ', end='')
+      if 'y' == input().lower():
+        print(f'ARE YOU ABSOLUTELY SURE YOU WANT TO DELETE {user[0]}????? (y/n): ', end='')
+        time.sleep(4)
+        if 'y' == input().lower():
+          run(f'sudo deluser {user[0]}')
+          doubleprint(f'        {user[0]} deleted\n')
+    else:
+      doubleprint(f"'{user[0]}' not in a login shell\n", sendtext=False)
+
+  input('Press anything to continue...')
 
 #--------------------Checking_Important_Groups---------------
-doubleprint(f"\n\nGroups:\n\n\n")
+  doubleprint(f"\n\nGroups:\n\n\n")
 
-groups = run('cat /etc/group')
+  groups = run('cat /etc/group')
 
-important_groups = ['samba', 'root', 'adm', 'sudo', 'crontab']
-for group in groups:
-  #print(group) 
-  group_name = group.split(':')[0]
-  #print(group_name)
-  if group_name in important_groups:
-    tab = '                      '
-  else:
+  important_groups = ['samba', 'root', 'adm', 'sudo', 'crontab']
+  for group in groups:
+    #print(group) 
+    group_name = group.split(':')[0]
+    #print(group_name)
     tab = ''
-  users_in_group = (group.split(':')[3:])
-  for user in users_in_group:
-    doubleprint(f"{tab}{group_name}: {user}\n")
-    
+    if group_name in important_groups:
+      tab = '                      '
+    users_in_group = (group.split(':')[3:])
+    for user in users_in_group:
+      doubleprint(f"{tab}{group_name}: {user}\n")
+  input('Press anything to continue...')
+
+if __name__ == '__main__':
+  main1()
